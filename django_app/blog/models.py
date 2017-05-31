341bb3757abeb518583d2c93697b825a3dccb317
ym4199 <ym4199@gmail.com>
2017-05-31 16:33:21 +0900

    add createsuperuser and add question

--- django_app/blog/models.py
+++ django_app/blog/models.py
@@ -1,3 +1,33 @@
+import datetime
+
+from django.utils import timezone
+
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    author = models.ForeignKey('auth.User', null=True)
+    question_title = models.CharField(max_length=200, null=True)
+    question_text = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now, null=True
+    )
+    pub_date = models.DateTimeField('date published',
+                                    blank=True, null=True)
+
+    def __str__(self):
+        return self.question_title
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
--- django_app/mysite/urls.py
+++ django_app/mysite/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+# from blog import views
+from blog import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index')
 ]
