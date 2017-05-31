456966982a5ee9004054dcf1108c51e5a99d822f
ym4199 <ym4199@gmail.com>
2017-05-31 17:39:25 +0900

    add templateand sort recently

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,7 +1,29 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Question
+
 
 def index(request):
-    return HttpResponse("text")
+    latest_question_list = Question.objects.order_by('-pub_date')[:]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'blog/question_list.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request,'blog/question_detail.html' ,{'question':question})
+
+def results(request, question_id):
+    response = "your looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+
--- django_app/mysite/urls.py
+++ django_app/mysite/urls.py
@@ -21,5 +21,8 @@ from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
